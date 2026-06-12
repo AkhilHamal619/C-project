adf8711429a6849856cd76e9044f0a19ac2fac1d
Akhilhamal619 <akhilhamal2005@gmail.com>
2026-06-12 12:10:09 +0545

    Update falsepoint.c

--- falsepoint.c
+++ falsepoint.c
@@ -1,5 +1,7 @@
-//False point Method
-#include<stdio.h>
+// false point method
+// solve 2-log(x) using false point method . E = 1e-5
+
+#include <stdio.h>
 #include <math.h>
 
 float f(float x)
@@ -9,18 +11,44 @@ float f(float x)
 
 int main()
 {
-    float a = 7, b = 8, E = 1e-10;
-    float c;
-    while(fabs(f(c) > E))
+    float a, b, c, E;
+    int i = 1;
+
+    printf("Enter the Tolerable Error: ");
+    scanf("%f", &E);
+
+    // Get valid interval
+    while (1)
     {
-        c = (a* f(b) - b * f(a))/f(b) - f(b);
-        printf("%f<n", c);
-        if(f(a) * f(c) < 0)
-        {
+        printf("Enter the lower limit of interval (a): ");
+        scanf("%f", &a);
 
-        }
-        else a = c;
+        printf("Enter the higher limit of interval (b): ");
+        scanf("%f", &b);
 
+        if (f(a) * f(b) < 0)
+            break;
+
+        printf("The root does not lie in this interval.\n");
+        printf("Please try again!\n\n");
     }
+
+    do
+    {
+        c = (a * f(b) - b * f(a)) / (f(b) - f(a));
+
+        printf("Iteration %d : %.6f\n", i, c);
+
+        if (f(a) * f(c) < 0)
+            b = c;
+        else
+            a = c;
+
+        i++;
+
+    } while (fabs(f(c)) > E);
+
+    printf("\nApproximate Root = %.6f\n", c);
+
     return 0;
 }
